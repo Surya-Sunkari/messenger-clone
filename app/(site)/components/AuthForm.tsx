c5c031b6c942940cbc08607385fc13edceaff61d
Surya Sunkari <suryasunkari@gmail.com>
2023-12-23 16:32:58 -0600

    added basis for users page and protected routes

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,20 +2,29 @@
 
 import Button from '@/app/components/Button';
 import Input from '@/app/components/inputs/Input';
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import AuthSocialButton from './AuthSocialButton';
 import {BsGithub, BsGoogle} from 'react-icons/bs';
 import axios from 'axios';
 import toast from 'react-hot-toast';
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { sign } from 'crypto';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
-    
+    const session = useSession();
+    const router = useRouter();
     const [variant, setVariant] = useState<Variant>('LOGIN');
     const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        if(session?.status === 'authenticated') {
+            router.push("/users")
+        }
+    }, [session?.status, router]);
 
     const toggleVariant = useCallback(() => {
         setVariant(variant === 'LOGIN' ? 'REGISTER' : 'LOGIN');
@@ -39,6 +48,7 @@ const AuthForm = () => {
         setIsLoading(true);
         if(variant === 'REGISTER') {
             axios.post('/api/register', data)
+            .then(() => signIn('credentials', data))
             .catch(() => toast.error('Something went wrong!'))
             .finally(() => setIsLoading(false));
         } 
@@ -55,6 +65,7 @@ const AuthForm = () => {
 
                 if(callback?.ok && !callback?.error) {
                     toast.success('Logged in successfully!');
+                    router.push("/users")
                 }
             })
             .finally(() => setIsLoading(false));
